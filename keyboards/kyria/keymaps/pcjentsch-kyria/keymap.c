3ac66b129d8ebf39a1667931753a911c76472895
pjentsch <pjentsch@uwaterloo.ca>
2021-05-12 21:34:16 -0400

    my keymap

--- keyboards/kyria/keymaps/pcjentsch-kyria/keymap.c
+++ keyboards/kyria/keymaps/pcjentsch-kyria/keymap.c
@@ -12,8 +12,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_RALT,   KC_A,   KC_S,  KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_RCTRL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   _______, _______, KC_DEL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENTER,
-                            KC_MUTE, KC_ESC,  KC_LGUI, MO(_LOWER),KC_LSFT,KC_SPC , MO(_RAISE), KC_BSPC  , KC_LALT, _______
+      KC_RCTRL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_1,   KC_2, KC_3, KC_4, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENTER,
+                            KC_MUTE, KC_ESC,  KC_LGUI, MO(_LOWER),KC_LSFT,MT(MOD_LSFT,KC_SPC) , MO(_RAISE), KC_BSPC  , KC_DEL, _______
     ),
     [_LOWER] = LAYOUT(
       _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_BSLS,
