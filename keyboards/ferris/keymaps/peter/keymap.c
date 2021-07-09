8a2ab242f68ac6c1924f451b022a3161d2fcd3ad
pjentsch <pjentsch@uwaterloo.ca>
2021-07-09 09:15:51 -0400

    add volup/down to ferris

--- keyboards/ferris/keymaps/peter/keymap.c
+++ keyboards/ferris/keymaps/peter/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT( 
      KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR,  KC_F11,   KC_F12,
    KC_EQL, KC_PLUS, KC_MINS, KC_LPRN, KC_RPRN,           LCTL_T(KC_LBRC), KC_RBRC,  KC_LCBR, KC_RCBR,KC_UNDERSCORE,
-    _______, _______, _______, _______, _______,                        KC_PIPE, KC_BSLASH, KC_GRAVE, KC_QUES , KC_SLASH,
+    _______, _______, _______, KC_VOLU, KC_VOLD,                        KC_PIPE, KC_BSLASH, KC_GRAVE, KC_QUES , KC_SLASH,
                                     _______, _______,                       _______, _______
   )
 };
